de1c28d5c2c4a71594339887529147c2db10b871
AidanaSaduova <aidana.saduova@gmail.com>
2019-04-20 22:07:21 +0200

    feat: implement verifyLogin in handler

--- app/handler/SimpleLoginHandler.java
+++ app/handler/SimpleLoginHandler.java
@@ -1,6 +1,7 @@
 package handler;
 
 import apimodels.UserLoginApiForm;
+import apimodels.UserViewModel;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import play.mvc.Http;
@@ -8,6 +9,7 @@ import play.mvc.Result;
 
 import java.io.IOException;
 
+import static play.mvc.Results.badRequest;
 import static play.mvc.Results.ok;
 
 public class SimpleLoginHandler extends LoginHandler {
@@ -23,6 +25,14 @@ public class SimpleLoginHandler extends LoginHandler {
     public Result verifyLogin(Http.Request request) throws IOException {
         JsonNode node = request.body().asJson();
         UserLoginApiForm form = mapper.readValue(node.toString(),UserLoginApiForm.class);
+
+        if(form.hasErrors()){
+            return badRequest(form.errorsAsJson());
+        }
+
+
+
+        UserViewModel user = new UserViewModel();
 
         return ok();
     }
