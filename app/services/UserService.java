60e247c669b390cc3abf8280f346800f4e884576
AidanaSaduova <aidana.saduova@gmail.com>
2019-04-22 15:53:49 +0200

    update: tried out to use classical finder instead of custom one

--- app/services/UserService.java
+++ app/services/UserService.java
@@ -1,16 +1,24 @@
 package services;
 
 import helper.UserFinder;
+import io.ebean.Expr;
+import io.ebean.Finder;
 import models.User;
 import org.mindrot.BCrypt;
 
 public class UserService {
 
     private static UserFinder find = new UserFinder();
+    private static Finder<Long,User> finder = new Finder<Long, User>(User.class);
+    private static final String FIELD_EMAIL = "email";
+    private static final String FIELD_USERNAME = "username";
+
 
     public User findByEmailorUsername(String name){
 
-        return find.byEmailOrName(name);
+        //return find.byEmailOrName(name);
+        return finder.query().where().or(Expr.eq(FIELD_EMAIL,name), Expr.eq(FIELD_USERNAME,name)).findOne();
+
     }
 
     public boolean checkPassword(User user, String password){
