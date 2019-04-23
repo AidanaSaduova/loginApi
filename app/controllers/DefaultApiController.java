9e8e00910a6c994a0b99e1cce87051777f2eaeed
AidanaSaduova <aidana.saduova@gmail.com>
2019-04-23 16:29:42 +0200

    update: Controllers to get right data

--- app/controllers/DefaultApiController.java
+++ app/controllers/DefaultApiController.java
@@ -1,15 +1,17 @@
 package controllers;
 
+import apimodels.UserLoginApiForm;
+import apimodels.UserViewModel;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.inject.Inject;
-import handler.LoginHandler;
-import handler.SimpleLoginHandler;
+import models.User;
 import play.Configuration;
-import play.Logger;
+import play.data.Form;
+import play.data.FormFactory;
 import play.libs.Json;
 import play.mvc.Controller;
-import play.mvc.Http;
 import play.mvc.Result;
+import services.UserService;
 import swagger.SwaggerUtils.ApiAction;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaPlayFrameworkCodegen", date = "2019-04-20T20:49:34.098+02:00")
@@ -19,11 +21,13 @@ public class DefaultApiController extends Controller {
     private final DefaultApiControllerImpInterface imp;
     private final ObjectMapper mapper;
     private final Configuration configuration;
+    private final FormFactory formFactory;
 
 
     @Inject
-    private DefaultApiController(Configuration configuration, DefaultApiControllerImpInterface imp) {
+    private DefaultApiController(Configuration configuration, DefaultApiControllerImpInterface imp, FormFactory formFactory) {
         this.imp = imp;
+        this.formFactory = formFactory;
         mapper = new ObjectMapper();
         this.configuration = configuration;
     }
@@ -32,15 +36,25 @@ public class DefaultApiController extends Controller {
     @ApiAction
     public Result login() throws Exception {
 
-        LoginHandler loginHandler;
-        Logger.debug("got a handler");
-        loginHandler = new SimpleLoginHandler();
+        Form<UserLoginApiForm> form = formFactory.form(UserLoginApiForm.class).bindFromRequest();
+        UserLoginApiForm userform = form.bindFromRequest().get();
 
-        Http.Request request = request();
+         if(form.hasErrors()){
+            return badRequest(form.errorsAsJson());
+        }
 
+        UserService us = new UserService();
+        User user = us.findByEmailorUsername(form.get().getName());
+        if(user==null){
 
-        Result result = loginHandler.verifyLogin(request);
-        Logger.debug(result.toString());
-        return  ok(Json.toJson(result));
+            return notFound();
+        }
+        if(!us.checkPassword(user, form.get().getPassword())){
+            return badRequest();
+        }
+
+        UserViewModel u = new UserViewModel(user);
+        return ok(Json.toJson(u));
+
     }
 }
